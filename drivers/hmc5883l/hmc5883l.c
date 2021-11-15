9954e0bf7b3f7a832f4e95c1f029099ca46aec11
Oleksandr Povshenko <oleksandr.povshenko@globallogic.com>
2021-11-15 18:04:31 +0200

    restructure driver fs

--- drivers/hmc5883l/hmc5883l.c
+++ drivers/hmc5883l/hmc5883l.c
@@ -1,11 +1,12 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/device.h>
+#include <linux/delay.h>
 #include <linux/err.h>
 #include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 
-#include "hmc5883l_driver.h"
+#include "hmc5883l.h"
 
 struct hmc5883l {
 	struct i2c_client *client;
@@ -23,17 +24,16 @@ static int hmc5883l_read_data(void)
 	if (drv_client == 0)
 		return -ENODEV;
 
-	hmc5883l.X_axis = i2c_smbus_read_byte_data(drv_client, REG_AXIS_X_LSB);
-	hmc5883l.X_axis |= i2c_smbus_read_byte_data(drv_client, REG_AXIS_X_MSB) << 8;
-	hmc5883l.Y_axis = i2c_smbus_read_byte_data(drv_client, REG_AXIS_Y_LSB);
-	hmc5883l.Y_axis |= i2c_smbus_read_byte_data(drv_client, REG_AXIS_Y_MSB) << 8;
-	hmc5883l.Z_axis = i2c_smbus_read_byte_data(drv_client, REG_AXIS_Z_LSB);
-	hmc5883l.Z_axis |= i2c_smbus_read_byte_data(drv_client, REG_AXIS_Z_MSB) << 8;
+	i2c_smbus_write_byte_data(drv_client, MODE_REG, SINGLE_MEASURE_MODE);
+	mdelay(6);
+	hmc5883l.X_axis = i2c_smbus_read_word_data(drv_client, REG_AXIS_X_MSB);
+	hmc5883l.Y_axis = i2c_smbus_read_word_data(drv_client, REG_AXIS_Y_MSB);
+	hmc5883l.Z_axis = i2c_smbus_read_word_data(drv_client, REG_AXIS_Z_MSB);
 
 	dev_info(&drv_client->dev, "read data:\n");
-	dev_info(&drv_client->dev, "X_axis: %02x\n", hmc5883l.X_axis);
-	dev_info(&drv_client->dev, "Y_axis: %02x\n", hmc5883l.Y_axis);
-	dev_info(&drv_client->dev, "Z_axis: %02x\n", hmc5883l.Z_axis);
+	dev_info(&drv_client->dev, "X_axis: %04x\n", hmc5883l.X_axis);
+	dev_info(&drv_client->dev, "Y_axis: %04x\n", hmc5883l.Y_axis);
+	dev_info(&drv_client->dev, "Z_axis: %04x\n", hmc5883l.Z_axis);
 
 	return 0;
 }
@@ -59,20 +59,41 @@ static ssize_t axis_Z_show(struct class *class, struct class_attribute *attr, ch
 	return strlen(buf);
 }
 
+static u8 verify_i2c_device(struct i2c_client *drv_client)
+{
+	if(i2c_smbus_read_byte_data(drv_client, IDENT_REG_A) == 'H' &&
+	   i2c_smbus_read_byte_data(drv_client, IDENT_REG_B) == '4' &&
+	   i2c_smbus_read_byte_data(drv_client, IDENT_REG_C) == '3')
+	   return 1;
+	
+	return 0;
+}
+
+static void set_device_mode(struct i2c_client *drv_client)
+{
+	i2c_smbus_write_byte_data(drv_client, CONFIG_REG_A, DEFAULT_REG_A);
+	i2c_smbus_write_byte_data(drv_client, CONFIG_REG_B, DEFAULT_REG_B);
+	i2c_smbus_write_byte_data(drv_client, MODE_REG, 	DEFAULT_MODE);
+}
+
 static int hmc5883l_probe(struct i2c_client *drv_client, const struct i2c_device_id *id)
 {
-	int reg = 0;
-	struct device_node *node;
+	// int reg = 0;
+	// struct device_node *node;
 	
 	dev_info(&drv_client->dev, "i2c client address is 0x%X\n", drv_client->addr);
 	dev_info(&drv_client->dev, "i2c driver probed\n");
 
+	if(!verify_i2c_device(drv_client))
+		return -ENODEV;
+
 	hmc5883l.client = drv_client;
+	set_device_mode(drv_client);
 	hmc5883l_read_data();
 
-	node =	drv_client->dev.of_node;
-	of_property_read_u32(node, "reg", &reg);
-	dev_info(&drv_client->dev, "reg = %d\n", reg);
+	// node =	drv_client->dev.of_node;
+	// of_property_read_u32(node, "reg", &reg);
+	// dev_info(&drv_client->dev, "reg = %d\n", reg);
 
 	return 0;
 }
